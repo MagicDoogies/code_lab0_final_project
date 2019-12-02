72726ed3d480d67f06451071bb2444fb6ce20152
MagicDoogies <fouronc@gmail.com>
2019-12-02 15:44:10 -0500

    lots of stuff
    fixed screen aspect ratio, created a script for button navigation, and created a general timer that will be used throughout the game and is flexible in how long it lasts.

---
+++ dog_sitter/Assets/scripts/countDownTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countDownTimer : MonoBehaviour
+{
+
+    float currentTime = 0f; //variable to hold what the current time of the timer is.
+
+    public float startingTime; //Can edit the starting time for my timer depending on the situation.
+
+    [SerializeField] Text countdownText; //Letting Unity know that the 'Text' box inside the object countdownText will be a field actively changed in this script.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+
+        
+        
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime; // Makes sure that the seconds go down by actual seconds as opposed to by frame.
+        countdownText.text = currentTime.ToString("0"); //As the numbers in the 'Text' box in game object countdownTimer change the new number will be displayed on screen.
+
+    }
+}
---
+++ dog_sitter/Assets/scripts/menuStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuStart : MonoBehaviour //The purpose of this script is to control all button navigation to different scenes.
+{
+  public void changemenuscene(string scenename)// Creaated a simple class that allows a Unity user to simply put in the names of the scene.
+    {
+        SceneManager.LoadScene(scenename);  //Telling the script that if I input the name of the scene in the "scenename" box then when the player clicks the button it wil load that scene.. 
+    }
+}
