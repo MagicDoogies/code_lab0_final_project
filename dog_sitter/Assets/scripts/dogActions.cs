93b20c2e8b2d78f3ed8f60cc632fa828d6ed88bd
MagicDoogies <fouronc@gmail.com>
2019-12-03 17:28:02 -0500

    some dog stuff
    Chorngus vibrates with apocolyptic energy

---
+++ dog_sitter/Assets/scripts/dogActions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dogActions : MonoBehaviour
+{
+    /*public float leftSpeed;
+    public int frames;
+    private bool changeDirection;
+    public float stopChanging = 1f;*/
+
+    Vector3 center;
+    public float centerDistance;
+    public float vibrateSpeed;
+
+    void Start()
+    {
+        center = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(center.x + Mathf.Sin(Time.time *vibrateSpeed) * centerDistance, center.y, center.z);
+
+    }
+}
---
+++ dog_sitter/Assets/scripts/pointTracking.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointTracking : MonoBehaviour
+{
+    public static int fedDog = 0;
+    public static int weekDay = 1;
+    public static int wrongChoice = 0;
+
+}
