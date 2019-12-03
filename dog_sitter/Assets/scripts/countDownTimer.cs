da3a0e3eba3d6c1ec15ddc6fa2d25649a2d26ce6
MagicDoogies <fouronc@gmail.com>
2019-12-03 00:05:46 -0500

    cleaned up timer
    cleaned up games wide timer. Tested out static variables will add them back in at a later time.
    Made sure that the timer stopped completely once it hit 0

--- dog_sitter/Assets/scripts/countDownTimer.cs
+++ dog_sitter/Assets/scripts/countDownTimer.cs
@@ -2,31 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class countDownTimer : MonoBehaviour
 {
+    private int sceneIndex = 0;
 
     float currentTime = 0f; //variable to hold what the current time of the timer is.
 
-    public float startingTime; //Can edit the starting time for my timer depending on the situation.
+    float startingTime = 8f;
+
+    private bool stopTimer= false;
 
     [SerializeField] Text countdownText; //Letting Unity know that the 'Text' box inside the object countdownText will be a field actively changed in this script.
 
     // Start is called before the first frame update
     void Start()
     {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
         currentTime = startingTime;
 
-        
-        
-   
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTime -= 1 * Time.deltaTime; // Makes sure that the seconds go down by actual seconds as opposed to by frame.
-        countdownText.text = currentTime.ToString("0"); //As the numbers in the 'Text' box in game object countdownTimer change the new number will be displayed on screen.
+        if (!stopTimer)
+        {
+            currentTime -= 1 * Time.deltaTime; // Makes sure that the seconds go down by actual seconds as opposed to by frame.
+            countdownText.text = currentTime.ToString("0"); //As the numbers in the 'Text' box in game object countdownTimer change the new number will be displayed on screen.
+        }
+
+        /*if (stopTimer)
+
+        {
+            currentTime = 0;
+        }*/
+
+      if (currentTime <= 0 && sceneIndex == 2)//if the timer reaches 0 AND the scene Index is 2- which is the index of the memorization screen.
+        {
+            SceneManager.LoadScene("main_game_room");//The scene manager will load in the scene titled main_game_room.
+        }
+
+      if (currentTime <= 0 && sceneIndex== 3 && !stopTimer)
+        {
+            print("DOG STARVED!!"); 
+            stopTimer = true;
+            
+        }
+    
 
     }
 }
