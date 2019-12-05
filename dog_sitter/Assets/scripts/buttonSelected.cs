571044e200c2eb3fd3e6f524def0e8da43cca11d
MagicDoogies <fouronc@gmail.com>
2019-12-05 00:04:51 -0500

    more stuff
    scripted in on button click events, fixed an error with duplicate audio listeners, moved scripts to more appropriate gameobjects, implemented base system of correct/wrong choices. About to test out functions to call various animations.

--- dog_sitter/Assets/scripts/buttonSelected.cs
+++ dog_sitter/Assets/scripts/buttonSelected.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 //handles button functions for whether or not they are right or wrong. 
 public class buttonSelected : MonoBehaviour
 {
-    public bool choice1 = false;
+   /* public bool choice1 = false;
     public bool choice2 = false;
-    public bool choice3 = false;
+    public bool choice3 = false;*/
    
     // Start is called before the first frame update
     void Start()
@@ -25,22 +26,101 @@ public class buttonSelected : MonoBehaviour
         
     }
     //functions public void Buttons 1-3 will give the buttons different onClick functions depending on the static variables in the pointTracking script. 
-    public void Button3()
+    public void Button1()
     {
-        choice3 = true;
-        
+        if (pointTracking.day == 1 && pointTracking.timesFed == 0)
+        {
+            print("It's day 1");
+            pointTracking.day = 2;
+            pointTracking.timesFed = 1;
+            SceneManager.LoadScene("main_game_room");
+            Invoke("DogPuke", 2.0f);
+            return;
+        }
+
+        if (pointTracking.day ==2 && pointTracking.timesFed == 1)
+        {
+            print("It's day 2");
+            pointTracking.day = 3;
+            pointTracking.timesFed = 2;
+            SceneManager.LoadScene("main_game_room");
+            return;
+        }
+
+        if (pointTracking.day == 3 && pointTracking.timesFed == 2)
+        {
+            pointTracking.day = 4;
+            pointTracking.timesFed = 3;
+            SceneManager.LoadScene("main_game_room");
+            return;
+        }
+
+
     }
     public void Button2()
     {
-        choice2 = true;
+        if (pointTracking.day == 1 && pointTracking.timesFed == 0)
+        {
+            print("It's day 1");
+            pointTracking.day = 2;
+            pointTracking.timesFed = 1;
+            SceneManager.LoadScene("main_game_room");
+            return;
+        }
+
+        if (pointTracking.day == 2 && pointTracking.timesFed == 1)
+        {
+            print("It's day 2");
+            pointTracking.day = 3;
+            pointTracking.timesFed = 2;
+            SceneManager.LoadScene("main_game_room");
+            return;
+        }
+
+        if (pointTracking.day == 3 && pointTracking.timesFed == 2)
+        {
+            pointTracking.day = 4;
+            print("it's day3");
+            pointTracking.timesFed = 3;
+            SceneManager.LoadScene("main_game_room");
+            return;
+        }
 
     }
 
-    public void Button1()
+    public void Button3()
     {
-        choice1 = true;
+        if (pointTracking.day == 1 && pointTracking.timesFed == 0)
+        {
+            print("It's day 1");
+            pointTracking.day = 2;
+            pointTracking.timesFed = 1;
+            SceneManager.LoadScene("main_game_room");
+            return;
+        }
 
+        if (pointTracking.day == 2 && pointTracking.timesFed == 1)
+        {
+            print("It's day 2");
+            pointTracking.day = 3;
+            pointTracking.timesFed = 2;
+            SceneManager.LoadScene("main_game_room");
+            return;
+        }
 
+        if (pointTracking.day == 3 && pointTracking.timesFed == 2)
+        {
+            print("It's day3 ");
+            pointTracking.day = 4;
+            pointTracking.timesFed = 3;
+            SceneManager.LoadScene("main_game_room");  
+            return;
+        }
+    }
+
+    public void DogPuke()
+    {
+        Debug.Log("Dog pukes");
     }
   
 }
--- dog_sitter/Assets/scripts/pointTracking.cs
+++ dog_sitter/Assets/scripts/pointTracking.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pointTracking : MonoBehaviour
 {
-    public static int timesFed = 1;
+    public static int timesFed = 0;
     public static int day = 1;
     public static int wrong = 0;
 
+   /* private bool oneDay = false;
+    private bool twoDay = false;
+    private bool threeDay = false;*/
+
     public static pointTracking instance = null;
+    private bool sceneLoaded = false;
     private void Awake()
     {
         if (instance == null)
@@ -20,6 +26,13 @@ public class pointTracking : MonoBehaviour
     {
         DontDestroyOnLoad(this.gameObject);
     }
-   
 
+    private void Update()
+    {
+      if (timesFed == 3 && day== 4 && !sceneLoaded)
+        {
+            SceneManager.LoadScene("score_screen");
+            sceneLoaded = true;
+        }
+    }
 }
